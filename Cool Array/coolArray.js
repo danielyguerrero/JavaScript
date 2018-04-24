0e0ac7fcf03dd9f255a29a323107fe8369230e4b
Danny <yanez.daniel0101@gmail.com>
2018-04-24 13:25:12 -0500

    added comments and condition

--- Cool Array/coolArray.js
+++ Cool Array/coolArray.js
@@ -1,22 +1,31 @@
-function coolArray(arr, symbol, reverse){
-    if (symbol == null){
-        symbol = "-->"
+
+function fancyPantsy(arr, symbol, reverse){
+// if symbol is null set it to a default
+    if(symbol == null){
+        symbol = "->";
     }
-    if (reverse == null){
-        for(let x = 0; x < arr.length; x++){
-            console.log(x + " " + symbol + " " + arr[x]);
+// if reverse is null OR it is false execute this
+    if (reverse == null || reverse == false){
+        for(let x = 0; x < arr.length; x ++){
+            console.log( x + " " + symbol + " " +arr[x]);
         }
     } else if (reverse == true){
-        for (let x = arr.length - 1; x >= 0; x--){
-            console.log(x + " " + symbol + " " + arr[x]);
+// Must arr.length -1 to account for 0 when counting backwards in arr; x >= 0 to stop @ 0
+        for(let x = arr.length-1; x >= 0; x --){
+            console.log( x + " " + symbol + " " + arr[x]);
         }
     }
+
 }
 
-coolArray(["Daniel", "Danny", "Dennis", "David"]);
+console.log("Symbol = null & Reverse = null: \n");
+fancyPantsy(["James", "Jill", "Jane", "Jack"]);
 
-console.log("custom symbol:")
-coolArray(["Daniel", "Danny", "Dennis", "David"], "=>");
+console.log("Custom symbol & Reverse = null: \n");
+fancyPantsy(["James", "Jill", "Jane", "Jack"], "=>");
 
-console.log('reverse order:');
-coolArray(["Daniel", "Danny", "Dennis", "David"], "=>", true)
+console.log("Custom Symbol & Reverse = true: \n")
+fancyPantsy(["danny", "david", "daniel", "dennis"], "==>", true);
+
+console.log("Custom Symbol: & reverse = false: \n")
+fancyPantsy(["danny", "david", "daniel", "dennis"], "=====>", false)
