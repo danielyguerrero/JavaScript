695dd35a27e13c9dc5e8cc8389660ad986242a8b
Daniel Y. Guerrero <yanez.daniel0101@gmail.com>
2018-05-09 08:09:47 -0500

    reverseString with built-in functions

--- Reverse String/reverseString.js
+++ Reverse String/reverseString.js
@@ -1,33 +1,13 @@
 // =========================================     METHOD 1    Reverse a String With Built-In Functions    =========================================
 
-// function reverseString(str) {
-//     // Step 1. Use the split() method to return a new array
-//     var splitString = str.split(""); // var splitString = "hello".split("");
-//     // ["h", "e", "l", "l", "o"]
- 
-//     // Step 2. Use the reverse() method to reverse the new created array
-//     var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-//     // ["o", "l", "l", "e", "h"]
- 
-//     // Step 3. Use the join() method to join all elements of the array into a string
-//     var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-//     // "olleh"
-    
-//     //Step 4. Return the reversed string
-//     return joinArray; // "olleh"
-// }
- 
-// reverseString("hello");
+function reverseString (str){
+	var arr = str.split(" ");
+	arr.reverse();
+	str = arr.join(" ");
+	console.log(str);
+}
 
-
-
-// function reverseString(str) {
-//     return str.split("").reverse().join("");
-// }
-// reverseString("hello");
-
-
-
+reverseString("Hello my name is Phill");
 
 // ============================================      METHOD 2     Reverse a String With a Decrementing For Loop      ========================
 
